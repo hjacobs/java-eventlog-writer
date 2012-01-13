d7f4926f7828538475e768dd2d88f6991447f939
Henning Jacobs <henning.jacobs@zalando.de>
2012-01-13 09:44:10 +0000

    REBOOT-11070 SingleDataSourceProvider

---
+++ zalando-sprocwrapper/src/main/java/de/zalando/sprocwrapper/dsprovider/SingleDataSourceProvider.java
@@ -0,0 +1,26 @@
+package de.zalando.sprocwrapper.dsprovider;
+
+import javax.sql.DataSource;
+
+public class SingleDataSourceProvider implements DataSourceProvider {
+    private DataSource dataSource;
+
+    public SingleDataSourceProvider() { }
+
+    public SingleDataSourceProvider(final DataSource ds) {
+        dataSource = ds;
+    }
+
+    public DataSource getDataSource(final int id) {
+        return dataSource;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+}
