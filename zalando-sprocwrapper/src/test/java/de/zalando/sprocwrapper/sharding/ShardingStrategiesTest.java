5ed9a1a8c05589e458e500312d66b6942d430855
Henning Jacobs <henning.jacobs@zalando.de>
2012-01-29 17:11:47 +0000

    REBOOT-11070 add Murmur2 based sharding (only for comparison)

--- zalando-sprocwrapper/src/test/java/de/zalando/sprocwrapper/sharding/ShardingStrategiesTest.java
+++ zalando-sprocwrapper/src/test/java/de/zalando/sprocwrapper/sharding/ShardingStrategiesTest.java
@@ -9,6 +9,45 @@ import org.junit.Test;
 public class ShardingStrategiesTest {
 
     @Test
+    public void testPerformance() {
+        VirtualShardKeyStrategy strategy1 = new VirtualShardKeyFromStringUsingMd5();
+        VirtualShardKeyStrategy strategy2 = new VirtualShardMurmur2HashStrategy();
+
+        Object[] args = new Object[1];
+
+        // sample SKU
+        args[0] = "SE622H003-802000S000";
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            strategy1.getShardId(args);
+        }
+
+        long between = System.currentTimeMillis();
+        for (int i = 0; i < 1000; i++) {
+            strategy2.getShardId(args);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("MD5:     " + (between - start) + " ms");
+        System.out.println("Murmur2: " + (end - between) + " ms");
+
+    }
+
+    @Test
+    public void testVirtualShardMurmur2HashStrategy() {
+        VirtualShardKeyStrategy strategy = new VirtualShardMurmur2HashStrategy();
+        Assert.assertEquals(0, strategy.getShardId(null));
+        Assert.assertEquals(0, strategy.getShardId(new Object[0]));
+        Assert.assertEquals(0, strategy.getShardId(new Object[1]));
+
+        Object[] args = new Object[1];
+        args[0] = "A";
+
+        Assert.assertTrue(strategy.getShardId(args) > 0);
+    }
+
+    @Test
     public void testVirtualShardKeyFromStringUsingMd5() {
         VirtualShardKeyStrategy strategy = new VirtualShardKeyFromStringUsingMd5();
         Assert.assertEquals(0, strategy.getShardId(null));
