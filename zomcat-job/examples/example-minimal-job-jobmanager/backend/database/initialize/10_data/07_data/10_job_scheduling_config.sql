48e3e09648050be0a0970fd0b628eea766ef1e1f
Daniel del Hoyo <daniel.del.hoyo@zalando.de>
2013-02-27 09:23:31 +0000

    PF-174 Changed cron entry in obmanager example

--- zomcat-job/examples/example-minimal-job-jobmanager/backend/database/initialize/10_data/07_data/10_job_scheduling_config.sql
+++ zomcat-job/examples/example-minimal-job-jobmanager/backend/database/initialize/10_data/07_data/10_job_scheduling_config.sql
@@ -3,7 +3,7 @@
 INSERT INTO zz_commons.job_scheduling_config(jsc_job_class, jsc_cron_expression, jsc_active, jsc_processing_limit, jsc_startup_processing_limit, jsc_app_instance_keys, jsc_job_data, jsc_job_group_config_id, jsc_description)
       VALUES
       -- Example Job, executes every 2 minutes
-       ('de.zalando.mentoring.example.jobs.ExampleJob', '0 0/2 * * * ?', TRUE,  1000, 50, ('{'|| 'local_local' ||'}')::text[], '{}', 1, 'Example job');
+       ('de.zalando.mentoring.example.jobs.ExampleJob', '0 0/2 * * * *', TRUE,  1000, 50, ('{'|| 'local_local' ||'}')::text[], '{}', 1, 'Example job');
 
 
 
