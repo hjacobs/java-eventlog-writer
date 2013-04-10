bece9436c6916e88903bb1ede26bd2b5438dcfae
Joerg Bellmann <joerg.bellmann@zalando.de>
2013-04-10 10:24:02 +0000

    ZEOS-14909 clean up

--- zomcat-jpa/trunk/zomcat-jpa-eclipselink/src/main/java/de/zalando/jpa/validation/AbstractConstraintViolationPrinter.java
+++ zomcat-jpa/trunk/zomcat-jpa-eclipselink/src/main/java/de/zalando/jpa/validation/AbstractConstraintViolationPrinter.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
+import com.google.common.base.Preconditions;
+
 /**
  * Builds an simple message and prints it to specific target.
  *
@@ -13,6 +15,9 @@ import javax.validation.ConstraintViolationException;
  * @see     {@link SystemErrConstraintViolationPrinter}
  */
 public abstract class AbstractConstraintViolationPrinter implements ConstraintViolationPrinter {
+
+    private ConstraintViolationMessageBuilder constraintViolationMessageBuilder =
+        new DefaultConstraintViolationMessageBuilder();
 
     @Override
     public void printValidationErrors(final Set<ConstraintViolation<?>> violations) {
@@ -23,30 +28,40 @@ public abstract class AbstractConstraintViolationPrinter implements ConstraintVi
     public abstract void printMessage(String message);
 
     protected String buildMessage(final Set<ConstraintViolation<?>> violations) {
-        StringBuilder sb = new StringBuilder("\n\n");
-        for (final ConstraintViolation<?> violation : violations) {
-            sb.append("\t[VALIDATION-ERROR]  ");
-            if (!violation.getRootBean().equals(violation.getLeafBean())) {
-                sb.append(violation.getRootBean()).append(" ");
-            }
-
-            sb.append(violation.getLeafBean().getClass().getSimpleName());
-            sb.append("#");
-            sb.append(violation.getPropertyPath().toString());
-            sb.append(" -- '");
-            sb.append(violation.getMessage());
-            sb.append("', but has value : ");
-            sb.append(violation.getInvalidValue());
-            sb.append(" on object : ").append(violation.getLeafBean().toString());
-            sb.append("\n");
-        }
-
-        return sb.toString();
+        return constraintViolationMessageBuilder.buildMessage(violations);
+            // StringBuilder sb = new StringBuilder("\n\n");
+            // for (final ConstraintViolation<?> violation : violations) {
+            // sb.append("\t[VALIDATION-ERROR]  ");
+            // if (!violation.getRootBean().equals(violation.getLeafBean())) {
+            // sb.append(violation.getRootBean()).append(" ");
+            // }
+            //
+            // sb.append(violation.getLeafBean().getClass().getSimpleName());
+            // sb.append("#");
+            // sb.append(violation.getPropertyPath().toString());
+            // sb.append(" -- '");
+            // sb.append(violation.getMessage());
+            // sb.append("', but has value : ");
+            // sb.append(violation.getInvalidValue());
+            // sb.append(" on object : ").append(violation.getLeafBean().toString());
+            // sb.append("\n");
+            // }
+            //
+            // return sb.toString();
     }
 
     @Override
     public void printValidationErrors(final ConstraintViolationException constraintViolationException) {
         printValidationErrors(constraintViolationException.getConstraintViolations());
+    }
+
+    public void setConstraintViolationMessageBuilder(
+            final ConstraintViolationMessageBuilder constraintViolationMessageBuilder) {
+
+        Preconditions.checkNotNull(constraintViolationMessageBuilder,
+            "ConstraintViolationMessageBuilder should not be null");
+
+        this.constraintViolationMessageBuilder = constraintViolationMessageBuilder;
     }
 
 }
