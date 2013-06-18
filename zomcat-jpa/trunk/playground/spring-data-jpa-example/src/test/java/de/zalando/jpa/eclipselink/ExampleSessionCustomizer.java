b8542e9eed02c41dbb5f2ddf1efbc2cbf020fd80
Joerg Bellmann <joerg.bellmann@zalando.de>
2013-06-18 14:31:18 +0000

    ASA-11 fix import

--- zomcat-jpa/trunk/playground/spring-data-jpa-example/src/test/java/de/zalando/jpa/eclipselink/ExampleSessionCustomizer.java
+++ zomcat-jpa/trunk/playground/spring-data-jpa-example/src/test/java/de/zalando/jpa/eclipselink/ExampleSessionCustomizer.java
@@ -1,5 +1,7 @@
 package de.zalando.jpa.eclipselink;
 
+import de.zalando.jpa.eclipselink.customizer.session.DefaultZomcatSessionCustomizer;
+
 public class ExampleSessionCustomizer extends DefaultZomcatSessionCustomizer {
 
     public ExampleSessionCustomizer() {
