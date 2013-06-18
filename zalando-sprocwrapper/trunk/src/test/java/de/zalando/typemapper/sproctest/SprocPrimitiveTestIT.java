492f1f56cf9350a7f94e924e51bd10080fd020ea
Daniel del Hoyo <daniel.del.hoyo@zalando.de>
2013-06-18 07:36:10 +0000

    PF-899 Init dbInitRegistry on IT

--- zalando-sprocwrapper/trunk/src/test/java/de/zalando/typemapper/sproctest/SprocPrimitiveTestIT.java
+++ zalando-sprocwrapper/trunk/src/test/java/de/zalando/typemapper/sproctest/SprocPrimitiveTestIT.java
@@ -55,7 +55,9 @@ public class SprocPrimitiveTestIT extends AbstractTest {
     @Test
     public void testPrimitivesWithSearchPath() throws SQLException {
         connection.createStatement().execute("set search_path to tmp2,tmp");
-        DbFunctionRegister.reInitRegistry(connection);
+
+        DbFunctionRegister db = new DbFunctionRegister(connection);
+        db.reInitRegistry(connection);
 
         final PreparedStatement ps = connection.prepareStatement("SELECT primitives_function();");
         final ResultSet rs = ps.executeQuery();
@@ -72,7 +74,9 @@ public class SprocPrimitiveTestIT extends AbstractTest {
     @Test
     public void testObjectArray() throws SQLException {
         connection.createStatement().execute("set search_path to tmp,tmp2");
-        DbFunctionRegister.reInitRegistry(connection);
+
+        DbFunctionRegister db = new DbFunctionRegister(connection);
+        db.reInitRegistry(connection);
 
         final PreparedStatement ps = connection.prepareStatement("SELECT tmp.array_function();");
         final ResultSet rs = ps.executeQuery();
