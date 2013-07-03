560f4507a1fca951df4fded3560715f762832df8
John Kliff Jochens <john.jochens@zalando.de>
2013-07-03 14:19:46 +0000

    ZEOS-19129 make job execution context available for finalizer as well

--- zomcat-batch/src/main/java/de/zalando/zomcat/jobs/batch/transition/AbstractBulkProcessingJob.java
+++ zomcat-batch/src/main/java/de/zalando/zomcat/jobs/batch/transition/AbstractBulkProcessingJob.java
@@ -247,7 +247,7 @@ public abstract class AbstractBulkProcessingJob<ITEM_TYPE> extends AbstractJob {
         LOG.trace("Using contexts to inject: local: {} job: {}",
             new Object[] {localJobExecutionContext, executionContext});
 
-        final Object[] jobSteps = new Object[] {fetcher, processor, writer};
+        final Object[] jobSteps = new Object[] {fetcher, processor, writer, finalizer};
         for (int i = 0; i < jobSteps.length; i++) {
             LOG.trace("Verifying need to inject on {}", jobSteps[i]);
             if (jobSteps[i] instanceof ExecutionContextAwareJobStep) {
