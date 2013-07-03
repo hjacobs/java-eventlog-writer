23015d3f223110fff1f23e82e38cb490894284a9
Evgeny Ryabitskiy <evgeny.ryabitskiy@zalando.de>
2013-07-03 12:37:17 +0000

    ZEOS-17789 - Payment: remove domain from reboot libs

--- zalando-payment-domain/src/main/java/de/zalando/payment/domain/settings/package-info.java
+++
@@ -1,5 +0,0 @@
-@XmlSchema(namespace = "http://payment.zalando.de/settings", elementFormDefault = XmlNsForm.QUALIFIED)
-package de.zalando.payment.domain.settings;
-
-import javax.xml.bind.annotation.XmlNsForm;
-import javax.xml.bind.annotation.XmlSchema;
