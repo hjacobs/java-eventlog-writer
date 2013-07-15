1ca80e3cfac4f2e574f2b633b18668e7e355fa67
Joerg Bellmann <joerg.bellmann@zalando.de>
2013-07-15 08:49:55 +0000

    ASA-22 fix build with stupid and not configurable maven-jacoco-plugin

---
+++ zomcat-jpa/trunk/playground/spring-data-sharded-jpa-example/src/main/java/OnlyExistForJacocoMavenPlugin.java
@@ -0,0 +1,11 @@
+
+/**
+ * This class only exist because the Maven-Jacoco-Plugin needs a directory named 'target/classes'. So if you do not have
+ * Sourcefiles under 'src/main/java' there will never be a 'classes'-directory under 'target'. So this is just a stupid
+ * way like all dummy-tests for UT-builds.<br/>
+ * <br/>
+ * I hate that way.
+ *
+ * @author  jbellmann
+ */
+public class OnlyExistForJacocoMavenPlugin { }
