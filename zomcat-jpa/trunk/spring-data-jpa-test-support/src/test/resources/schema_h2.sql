c794ee6ee69ebcd5a5d911af252002aee99df53e
Joerg Bellmann <joerg.bellmann@zalando.de>
2013-07-25 08:24:20 +0000

    ASA-22 comment added to schema

--- zomcat-jpa/trunk/spring-data-jpa-test-support/src/test/resources/schema_h2.sql
+++ zomcat-jpa/trunk/spring-data-jpa-test-support/src/test/resources/schema_h2.sql
@@ -1,2 +1,3 @@
+-- this is for sharding-config-test, because seems to be better to setup the schema on all shards at same time
 CREATE SCHEMA IF NOT EXISTS zzj_data AUTHORIZATION sa;
 CREATE SCHEMA IF NOT EXISTS zzj_api AUTHORIZATION sa;
