0e3c23a69aefa68e7340e081b6c179b047e55399
Sean Floyd <sean.floyd@zalando.de>
2014-02-04 15:20:22 +0000

    ZEOS-29169 project is now in stash/shop

--- zalando-urlmapping/src/main/java/de/zalando/util/web/urlmapping/RequestParamAware.java
+++
@@ -1,7 +0,0 @@
-package de.zalando.util.web.urlmapping;
-
-import com.google.common.collect.ListMultimap;
-
-public interface RequestParamAware {
-    boolean appliesTo(ListMultimap<String, String> parameterMap);
-}
